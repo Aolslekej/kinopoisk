da4b0c249057777eb15e95b136a8667b882dbd4f
Aolslekej <alexander_baranovsky@mail.ru>
2023-12-23 15:20:06 +0300

    new kinopoisk

--- src/pages/FilmPage/FilmPage.jsx
+++ src/pages/FilmPage/FilmPage.jsx
@@ -1,10 +1,14 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import "./filmpage.css";
 
 export default function FilmPage() {
   return (
     <div>
       <div className="container">
+        <Link to="/">
+          <button>Back</button>
+        </Link>
         <h2 className="filmName">Name</h2>
         <div className="items">
           <h3 className="age">15+</h3>
